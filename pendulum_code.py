30e5fc27c897a451d63b4fcc8c99961790f1b703
Adil Faisal <adilfaisal01@gmail.com>
2025-09-27 23:21:57 -0400

    initial gym env written

--- pendulum_code.py
+++ pendulum_code.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
+import gymnasium as gym
+from gymnasium.spaces import Box
 
 # # system parameter values-- can be iterated upon
 # L=0.3810 # length of pendulum in meters
@@ -14,25 +14,84 @@ import pandas as pd
 # I_arm=1/3*m*L**2  #moment of inertia
 # print(f' moment of inertia for the arm: {I_arm}')
 
-class SpringInvertedPendulum:
-    def __init__(self,M,k_spring,b):
+class SpringInvertedPendulum(gym.Env):
+    metadata = {
+        "render_modes": ["human", "rgb_array"],
+        "render_fps": 50,
+    }
+
+    def __init__(self,M,k_spring,max_steps=500,render_mode: str | None=None):
         self.g=9.81 #gravity acceleration in m/s^2
         self.m=0.59787 #arm mass in kg
         self.L=0.3810 # arm length in meters
         self.M=M #masses being placed on top
         self.k_spring=k_spring #spring placed on top
         self.I=1/3*self.m*self.L**2+self.M*self.L**2 #moment of inertia
-        self.b=2*np.sqrt((self.k_spring-0.5*self.m*self.g*self.L-self.M*self.g*self.L)*self.I)*(1+0.3*np.random.rand()) #adding random ness to the critical damping for better physics randomization
+        self.b=2*np.sqrt((self.k_spring-0.5*self.m*self.g*self.L-self.M*self.g*self.L)*self.I) #adding random ness to the critical damping for better physics randomization
         self.p=self.m*self.L/2+self.M*self.L 
         self.dt=0.01 #time interval (in seconds)
         self.t=0 #initial time
 
-    def physics(self,theta,t_motor,theta_dot):
+
+        # action space, defined as [-1.0,1.0] nm from the motor
+        self.action_space=Box(low=-1.0,high=1.0,shape=(1,),dtype=np.float64)
+        # state space--> angular velocity and angle of the pendulum arm
+        high_obs=np.array([np.pi/2, np.inf])
+        self.observation_space=Box(low=-high_obs,high=high_obs, shape=(2,),dtype=np.float64)
+        # initialize the physics
+        self.state: np.array=np.zeros(shape=(2,))
+        self.terminated=False
+        self.maxsteps=max_steps
+        self.current_steps=0
+
+    # # def physics(self,theta,t_motor,theta_dot):
+    # #     theta_ddot=(self.p*self.g*np.sin(theta)+t_motor-self.k_spring*theta-self.b*theta_dot)/self.I #governing equation of the system, found through Euler-Lagrangian method
+    # #     theta_dot=theta_dot+theta_ddot*self.dt
+    # #     theta=theta+theta_dot*self.dt
+
+    #     return theta_dot,theta
+    def step(self, action):
+
+        theta,theta_dot=self.state #previous state
+        t_motor=float(action[0]) #action taken
+        
+        #move the physics forward by dt
         theta_ddot=(self.p*self.g*np.sin(theta)+t_motor-self.k_spring*theta-self.b*theta_dot)/self.I #governing equation of the system, found through Euler-Lagrangian method
         theta_dot=theta_dot+theta_ddot*self.dt
         theta=theta+theta_dot*self.dt
+        self.state=np.array([theta,theta_dot]) #updated states
+        # check for termination
+        termination=bool(theta>np.pi/2 or theta <-np.pi/2)
+        if termination:
+            self.terminated=True #arm moved too far, get wreckt lol
+        else:
+            self.terminated=False
+        reward=-5*np.abs(theta) - 0.1 * theta_dot**2 #reward function
+        self.current_steps=self.current_steps+1
+        if self.current_steps>=self.maxsteps:
+            truncated=True
+        else:
+            truncated=False
+        return np.array(self.state,dtype=np.float64),reward,termination,truncated,{}
+    
+    def reset(self,seed=None,options=None):
+        super().reset(seed=seed)
+        self.state=self.np_random.uniform(low=np.array([-0.3,-1]),high=np.array([0.3,1]),size=(2,))
+        self.k_spring=np.random.choice(np.array([20.492,31.446,42,50.446,61,80])*0.11298)
+        self.terminated=False
+        self.current_steps=0
+        return np.array(self.state,dtype=np.float64),{}
+    def render(self, mode="human"):
+        None
 
-        return theta_dot,theta
+
+
+        
+    
+
+
+
+       
 
 
 
